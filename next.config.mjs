e22e2b45738180999dfd6fc49d3fb841f5dde6de
Sachin Kumar <sachinrajputlive@gmail.com>
2025-05-24 15:12:10 +0530

    [main] base url added for tailwind configration

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,7 @@ const nextConfig = {
     output: 'export',
     basePath: `/${repoName}`,
     assetPrefix: `/${repoName}/`,
+    basePath: process.env.NODE_ENV === 'production' ? '/mcq-app' : '',
 };
 
 export default nextConfig;
