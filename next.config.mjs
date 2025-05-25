edf3e11cf7382c89b7c9de1d2e3125262e40344b
Sachin Kumar <sachinrajputlive@gmail.com>
2025-05-25 12:25:45 +0530

    [main] base url fixes

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
     output: 'export',
     distDir: 'out',
     basePath: process.env.NODE_ENV === 'production' ? `/${repoName}` : "",
-    assetPrefix: process.env.NODE_ENV === 'production' ? `/${repoName}` : "",
+    assetPrefix: process.env.NODE_ENV === 'production' ? `/${repoName}/` : "",
 };
 
 export default nextConfig;
