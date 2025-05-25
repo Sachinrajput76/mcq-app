ffbe9df97da245b481e0aec8c9773df280382126
Sachin Kumar <sachinrajputlive@gmail.com>
2025-05-25 12:30:22 +0530

    [main] base url fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
-const repoName = 'mcq-app';
 
 const nextConfig = {
     output: 'export',
-    distDir: 'out',
-    basePath: process.env.NODE_ENV === 'production' ? `/${repoName}` : "",
-    assetPrefix: process.env.NODE_ENV === 'production' ? `/${repoName}/` : "",
+    basePath: '/mcq-app',
+    assetPrefix: '/mcq-app/',
 };
 
 export default nextConfig;
